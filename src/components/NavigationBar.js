99550342b22e2e74fa989d5e3d4b1b0e8f4752e9
Andrew Wei <andrewwei007@gmail.com>
2023-09-19 20:19:02 -0400

    feat: create navigation bar

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import {Link} from 'react-router-dom';
+import '../styles/NavBar.css';
+
+function NavigationBar() {
+  return(
+    <div className='navbar'>
+      <Box className='container'>
+
+        <Link className='link' to="/">Home</Link>
+        <Link className='link' to="/experience">Experience</Link>
+
+      </Box>
+    </div>
+  )
+}
+
+export default NavigationBar;
